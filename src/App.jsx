09435e618cf49df6e9e41fbf1d2419f6d595daac
NAOT0 <NAOT0@example.com>
2025-01-28 22:29:59 +0900

    データとブラウザの同期

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // このタスクが編集されたタスクと同じ ID を持っている場合
+      if (id === task.id) {
+        // オブジェクトを開いて、 `completed` プロップが
+        // 反転された新しいオブジェクトを作成します。
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
+
   function addTask(name) {
-    alert(name);
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
   }
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -25,7 +46,8 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
+
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
